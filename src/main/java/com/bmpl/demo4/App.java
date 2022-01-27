736b6c348fbdba95c84dadceb2e40bef2eed5dc0
Rammehar <rammehar1981@gmail.com>
2022-01-27 12:43:19 +0530

    starting the new project

---
+++ src/main/java/com/bmpl/demo4/App.java
@@ -0,0 +1,17 @@
+package com.bmpl.demo4;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
